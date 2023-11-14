8a473ed6ffa2a68a5e548040c82e0d5f85327461
sathvi7 <sathvika2505@gmail.com>
2023-11-14 21:36:31 +0530

    Adding CMS Project

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,10 @@
 <?php 
 
-$connect = mysqli_connect('mysql.db.mdbgo.com', 'dawidadach_cmsdb', 'Secret.1','dawidadach_cmsdb');
+$connect = mysqli_connect('mysql.db.mdbgo.com', 'sathvika7_cmsdb', 'Ppassword$1','sathvika7_cmsdb');
 
 if (mysqli_connect_errno()){
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+
+//mysql.db.mdbgo.com
+//'mysql.db.mdbgo.com', 'dawidadach_cmsdb', 'Secret.1','dawidadach_cmsdb'
