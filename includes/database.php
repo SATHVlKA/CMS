34cf098da943c163ec08d2d3399a44082be74b4f
sathvi7 <sathvika2505@gmail.com>
2023-12-11 21:23:22 +0530

    second commit

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php 
 
-$connect = mysqli_connect('mysql.db.mdbgo.com', 'sathvika7_cmsdb', 'Ppassword$1','sathvika7_cmsdb');
+$connect = mysqli_connect('mysql.db.mdbgo.com', 'sathvika2505_cmsdb', 'Password@1','sathvika2505_cmsdb');
 
 if (mysqli_connect_errno()){
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
@@ -8,3 +8,6 @@ if (mysqli_connect_errno()){
 
 //mysql.db.mdbgo.com
 //'mysql.db.mdbgo.com', 'dawidadach_cmsdb', 'Secret.1','dawidadach_cmsdb'
+
+
+//mysql://sathvika2505_cmsdb:Password$1@mysql.db.mdbgo.com:3306/sathvika2505_cmsdb
