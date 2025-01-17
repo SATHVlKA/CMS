03a68c8a7814cebe0745706f5541cd424d0dec7f
Sathvika K <123883498+SATHVlKA@users.noreply.github.com>
2025-01-17 17:33:14 +0530

    Commit Changes

--- includes/database.php
+++ includes/database.php
@@ -6,8 +6,6 @@ if (mysqli_connect_errno()){
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-//mysql.db.mdbgo.com
-//'mysql.db.mdbgo.com', 'dawidadach_cmsdb', 'Secret.1','dawidadach_cmsdb'
 
 
 //mysql://sathvika2505_cmsdb:Password$1@mysql.db.mdbgo.com:3306/sathvika2505_cmsdb
